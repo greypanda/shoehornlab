31b79208fcc845c00771bc089b92edd4938ee583
John Crunk <john@pandavista.com>
2018-01-01 15:52:27 -0500

    axxx

---
+++ bin/Config.py
@@ -0,0 +1,7 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- bin/rawapp.py
+++ bin/rawapp.py
@@ -1,9 +1,6 @@
 import logging
 from nmap_scan import nmapscan
-try:
-    import ConfigParser 
-except:
-    import configparser as ConfigParser
+import ConfigParser
 import sys
 import os
 from inventory import Inventory
@@ -16,11 +13,8 @@ def main():
     config.read('../etc/' + root + '.ini')
     logging.basicConfig(filename=config.get('logging','dir')+ root + '.log',format='%(asctime)s:%(levelname)s:%(message)s',level=logging.DEBUG)
     if nmapscan(config):
-        print( 'OK')
+        print 'OK'
     inv = Inventory(config,root)
     inv.parse()
 if __name__ == "__main__":
-    if not sys.version_info[:2] >= (3,5):
-        print('Python 3.5 or greater required')
-        sys.exit(2)
     main()
