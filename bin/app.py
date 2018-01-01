64fbca6d84e4a87a3b2d1ab07e78b2a675d049a0
John Crunk <john@pandavista.com>
2018-01-01 16:13:30 -0500

    fixed lint messages

--- bin/app.py
+++ bin/app.py
@@ -1,26 +1,32 @@
+"""Run nmap and parse into the database.
+
+"""
+
 import logging
-from nmap_scan import nmapscan
-try:
-    import ConfigParser 
-except:
-    import configparser as ConfigParser
+import configparser as ConfigParser
 import sys
 import os
+from nmap_scan import nmapscan
 from inventory import Inventory
 
 def main():
+    """Main.
+
+    """
     config = ConfigParser.ConfigParser()
-    base =  os.path.basename(__file__)
+    base = os.path.basename(__file__)
     root = os.path.splitext(base)[0]
 
     config.read('../etc/' + root + '.ini')
-    logging.basicConfig(filename=config.get('logging','dir')+ root + '.log',format='%(asctime)s:%(levelname)s:%(message)s',level=logging.DEBUG)
+    logging.basicConfig(filename=config.get('logging', 'dir')+ root + '.log',
+                        format='%(asctime)s:%(levelname)s:%(message)s',
+                        level=logging.DEBUG)
     if nmapscan(config):
-        print( 'OK')
-    inv = Inventory(config,root)
+        print('OK')
+    inv = Inventory(config, root)
     inv.parse()
 if __name__ == "__main__":
-    if not sys.version_info[:2] >= (3,5):
+    if not sys.version_info[:2] >= (3, 5):
         print('Python 3.5 or greater required')
         sys.exit(2)
     main()
--- bin/nmap_scan.py
+++ bin/nmap_scan.py
@@ -19,22 +19,22 @@ def nmapscan(config):
         nmaptask = nmap_proc.current_task
         if nmaptask:
             logging.info("Task {0} ({1}):DONE: {2}%".format(nmaptask.name,
-                                                               nmaptask.status,
-                                                               nmaptask.progress))
+                                                            nmaptask.status,
+                                                            nmaptask.progress))
 
 
 
-    nmap_proc = NmapProcess(targets=config.get('nmap','targets'),
-                            options=config.get('nmap','options'),
+    nmap_proc = NmapProcess(targets=config.get('nmap', 'targets'),
+                            options=config.get('nmap', 'options'),
                             event_callback=mycallback)
     logging.info("Starting nmap scan")
     nmap_proc.sudo_run()
     logging.info("Ending nmap scan")
-    if os.path.isfile(config.get('nmap','xml')):
+    if os.path.isfile(config.get('nmap', 'xml')):
         logging.info("Moving xml to xml-backup")
-        os.rename(config.get('nmap','xml'),config.get('nmap','xml-backup'))
-    xml = open(config.get('nmap','xml'),'w')
+        os.rename(config.get('nmap', 'xml'), config.get('nmap', 'xml-backup'))
+    xml = open(config.get('nmap', 'xml'), 'w')
     xml.write(nmap_proc.stdout)
     xml.close()
-    logging("nmap scan complete")
+    logging.info("nmap scan complete")
     return True
